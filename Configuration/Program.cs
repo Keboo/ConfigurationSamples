311afdeaa3e83a8a16accf4c299cc331da1218ee
Kevin Bost <kitokeboo@gmail.com>
2024-07-09 13:40:12 -0700

    Adding generic host
    Adding IOptions
    Adding validation

--- Configuration/Program.cs
+++ Configuration/Program.cs
@@ -1,26 +1,48 @@
 ﻿// See https://aka.ms/new-console-template for more information
 using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Options;
+using System.ComponentModel.DataAnnotations;
 
-ConfigurationBuilder builder = new();
 
-builder.AddInMemoryCollection(new Dictionary<string, string?>
-{
-    ["WebServices:serviceUrl"] = "http://myurl.com"
-});
-builder.AddJsonFile("appsettings.json", optional: true, reloadOnChange: true);
-builder.AddCommandLine(args);
 
-IConfiguration configuration = builder.Build();
+using IHost host = Host.CreateDefaultBuilder()
+    .ConfigureHostConfiguration(config =>
+    {
+        //IConfiguration for the host itself
+    })
+    .ConfigureAppConfiguration((hostingContext, config) =>
+    {
+        config.AddInMemoryCollection(new Dictionary<string, string?>
+        {
+            ["WebServices:ServiceUrl"] = "http://myurl.com"
+        });
+    })
+    .ConfigureServices(services =>
+    {
+        services.AddOptions<WebServices>()
+                .BindConfiguration(WebServices.SectionName)
+                .ValidateDataAnnotations()
+                .ValidateOnStart();
+    })
+    .Build();
 
 do
 {
-    WebServices? webServices = configuration.GetSection("WebServices").Get<WebServices>();
+    IOptions<WebServices> webServices = host.Services.GetRequiredService<IOptions<WebServices>>();
 
-    Console.WriteLine($"Service URL: {webServices?.ServiceUrl}");
+    Console.WriteLine($"Service URL: {webServices.Value.ServiceUrl}");
 
     Console.WriteLine("Press ESC to exit");
 } while (Console.ReadKey().Key != ConsoleKey.Escape);
 
 
 
-public sealed record WebServices(Uri ServiceUrl);
+public sealed record WebServices
+{
+    public const string SectionName = "WebServices";
+
+    [Required]
+    public required Uri ServiceUrl { get; set; }
+}

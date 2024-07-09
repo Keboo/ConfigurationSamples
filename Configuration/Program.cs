ebc4ce328d501b7fdb1160d5077cdc17e4d78187
Kevin Bost <kitokeboo@gmail.com>
2024-07-08 23:29:18 -0700

    Adding JSON file source

--- Configuration/Program.cs
+++ Configuration/Program.cs
@@ -2,13 +2,20 @@
 using Microsoft.Extensions.Configuration;
 
 ConfigurationBuilder builder = new();
+
 builder.AddInMemoryCollection(new Dictionary<string, string?>
 {
     ["serviceUrl"] = "http://myurl.com"
 });
+builder.AddJsonFile("appsettings.json", optional: true, reloadOnChange: true);
 builder.AddCommandLine(args);
 
 IConfigurationRoot configuration = builder.Build();
 
-string? serviceUrl = configuration["serviceUrl"];
-Console.WriteLine($"Service URL: {serviceUrl}");
+do
+{
+    string? serviceUrl = configuration["serviceUrl"];
+    Console.WriteLine($"Service URL: {serviceUrl}");
+
+    Console.WriteLine("Press ESC to exit");
+} while (Console.ReadKey().Key != ConsoleKey.Escape);

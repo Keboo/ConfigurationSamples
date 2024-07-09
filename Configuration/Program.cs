a6fa971a69dd8f2a57ccb812ed25c4b22820aa0c
Kevin Bost <kitokeboo@gmail.com>
2024-07-08 22:59:07 -0700

    Basic IConfiguration

---
+++ Configuration/Program.cs
@@ -0,0 +1,13 @@
+﻿// See https://aka.ms/new-console-template for more information
+using Microsoft.Extensions.Configuration;
+
+ConfigurationBuilder builder = new();
+builder.AddInMemoryCollection(new Dictionary<string, string?>
+{
+    ["serviceUrl"] = "http://myurl.com"
+});
+
+IConfigurationRoot configuration = builder.Build();
+
+string? serviceUrl = configuration["serviceUrl"];
+Console.WriteLine($"Service URL: {serviceUrl}");
